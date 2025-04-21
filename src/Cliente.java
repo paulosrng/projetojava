bdc96a82e2cf69f0701d9fb490f4c3aebf68fc45
paulosrng <paulosrngbusiness@gmail.com>
2025-04-21 20:49:17 -0300

    adicionar Cliente

---
+++ src/Cliente.java
@@ -0,0 +1,12 @@
+
+public abstract class Cliente {
+    protected String nome;
+    protected String telefone;
+    protected String email;
+
+    public Cliente(String nome, String telefone, String email) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+    }
+}
