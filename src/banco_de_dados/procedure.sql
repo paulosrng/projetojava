3a9b0649a8a625e6c22cc1c69fbb741352f83ba6
Augusto <augustoramos067@gmail.com>
2025-04-21 23:04:38 -0300

    procedure 3

--- src/banco_de_dados/procedure.sql
+++ src/banco_de_dados/procedure.sql
@@ -41,3 +41,27 @@ BEGIN
     VALUES (p_cliente_id, p_pacote_id, p_data_viagem, v_pacote_preco, 'PENDENTE');
     
     SET v_pedido_id = LAST_INSERT_ID();
+
+    -- Adicionar serviços (se houver)
+    WHILE i < JSON_LENGTH(p_servicos_json) DO
+        SET v_servico_id = JSON_EXTRACT(p_servicos_json, CONCAT('$[', i, '].servico_id'));
+        SET v_quantidade = JSON_EXTRACT(p_servicos_json, CONCAT('$[', i, '].quantidade'));
+        
+        INSERT INTO PedidoServico (pedido_id, servico_id, quantidade, preco_unitario)
+        SELECT v_pedido_id, v_servico_id, v_quantidade, preco
+        FROM ServicoAdicional
+        WHERE id = v_servico_id;
+        
+        SET i = i + 1;
+    END WHILE;
+    
+    -- Atualizar valor total
+    UPDATE Pedido
+    SET valor_total = (
+        SELECT v_pacote_preco + COALESCE(SUM(ps.preco_unitario * ps.quantidade), 0)
+        FROM PedidoServico ps
+        WHERE ps.pedido_id = v_pedido_id
+    )
+    WHERE id = v_pedido_id;
+END //
+DELIMITER ;
