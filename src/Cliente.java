d3d2e147c98ed7f41494da4423da24fdcc1c57b9
paulosrng <paulosrngbusiness@gmail.com>
2025-04-21 22:46:23 -0300

    ajustes para rodar o codigo

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public abstract class Cliente {
     protected String nome;
@@ -9,70 +11,74 @@ public abstract class Cliente {
         this.telefone = telefone;
         this.email = email;
     }
+
     public abstract String getDocumento();
 }
-public class ClienteNacional extends Cliente {
+
+class ClienteNacional extends Cliente {
     private String cpf;
 
     public ClienteNacional(String nome, String telefone, String email, String cpf) {
         super(nome, telefone, email);
         this.cpf = cpf;
     }
-}
+
     @Override
     public String getDocumento() {
         return cpf;
     }
-    public class ClienteEstrangeiro extends Cliente {
-        private String passaporte;
-    
-        public ClienteEstrangeiro(String nome, String telefone, String email, String passaporte) {
-            super(nome, telefone, email);
-            this.passaporte = passaporte;
-        }
-    
-        @Override
-        public String getDocumento() {
-            return passaporte;
-        }
-    }
-    public abstract class PacoteViagem {
-        protected String nome;
-        protected String destino;
-        protected int duracao;
-        protected double preco;
-        protected String tipo;
-    
-        public PacoteViagem(String nome, String destino, int duracao, double preco, String tipo) {
-            if (destino == null || destino.isEmpty() || preco <= 0) {
-                throw new IllegalArgumentException("Pacote deve conter destino e preço válido");
-            }
-            this.nome = nome;
-            this.destino = destino;
-            this.duracao = duracao;
-            this.preco = preco;
-            this.tipo = tipo;
-        }
-        
-    }
-    public abstract class ServicoAdicional {
-        protected String nome;
-        protected double preco;
-    
-        public ServicoAdicional(String nome, double preco) {
-            this.nome = nome;
-            this.preco = preco;
-        }
-        
+}
+
+class ClienteEstrangeiro extends Cliente {
+    private String passaporte;
+
+    public ClienteEstrangeiro(String nome, String telefone, String email, String passaporte) {
+        super(nome, telefone, email);
+        this.passaporte = passaporte;
     }
 
-public class Passeio extends ServicoAdicional {
+    @Override
+    public String getDocumento() {
+        return passaporte;
+    }
+}
+
+abstract class PacoteViagem {
+    protected String nome;
+    protected String destino;
+    protected int duracao;
+    protected double preco;
+    protected String tipo;
+
+    public PacoteViagem(String nome, String destino, int duracao, double preco, String tipo) {
+        if (destino == null || destino.isEmpty() || preco <= 0) {
+            throw new IllegalArgumentException("Pacote deve conter destino e preço válido");
+        }
+        this.nome = nome;
+        this.destino = destino;
+        this.duracao = duracao;
+        this.preco = preco;
+        this.tipo = tipo;
+    }
+}
+
+abstract class ServicoAdicional {
+    protected String nome;
+    protected double preco;
+
+    public ServicoAdicional(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+}
+
+class Passeio extends ServicoAdicional {
     public Passeio(String nome, double preco) {
         super(nome, preco);
     }
 }
 
-public class Pedido {
+class Pedido {
     private Cliente cliente;
     private PacoteViagem pacote;
     private List<ServicoAdicional> servicos;
@@ -86,6 +92,4 @@ public class Pedido {
     public void adicionarServico(ServicoAdicional servico) {
         servicos.add(servico);
     }
-
-    
 }
