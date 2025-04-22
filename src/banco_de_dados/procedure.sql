754ef7101232324c95253652dde61e9b6f0ebf6b
Augusto <augustoramos067@gmail.com>
2025-04-21 23:04:06 -0300

    procedure 2

--- src/banco_de_dados/procedure.sql
+++ src/banco_de_dados/procedure.sql
@@ -18,3 +18,26 @@ BEGIN
 END //
 DELIMITER ;
 
+-- Procedure para criar um novo pedido com serviços
+DELIMITER //
+CREATE PROCEDURE sp_criar_pedido(
+    IN p_cliente_id INT,
+    IN p_pacote_id INT,
+    IN p_data_viagem DATE,
+    IN p_servicos_json JSON
+)
+BEGIN
+    DECLARE v_pacote_preco DECIMAL(10,2);
+    DECLARE v_pedido_id INT;
+    DECLARE i INT DEFAULT 0;
+    DECLARE v_servico_id INT;
+    DECLARE v_quantidade INT;
+    
+    -- Obter preço do pacote
+    SELECT preco INTO v_pacote_preco FROM PacoteViagem WHERE id = p_pacote_id;
+    
+    -- Criar o pedido
+    INSERT INTO Pedido (cliente_id, pacote_id, data_viagem, valor_total, status)
+    VALUES (p_cliente_id, p_pacote_id, p_data_viagem, v_pacote_preco, 'PENDENTE');
+    
+    SET v_pedido_id = LAST_INSERT_ID();
