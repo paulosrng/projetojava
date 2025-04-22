ce37ff559a5829622eccfdfc0a42c0615a00aa60
paulosrng <paulosrngbusiness@gmail.com>
2025-04-21 22:01:17 -0300

    Classe Cliente Estrangeiro

--- src/Cliente.java
+++ src/Cliente.java
@@ -23,3 +23,16 @@ public class ClienteNacional extends Cliente {
     public String getDocumento() {
         return cpf;
     }
+    public class ClienteEstrangeiro extends Cliente {
+        private String passaporte;
+    
+        public ClienteEstrangeiro(String nome, String telefone, String email, String passaporte) {
+            super(nome, telefone, email);
+            this.passaporte = passaporte;
+        }
+    
+        @Override
+        public String getDocumento() {
+            return passaporte;
+        }
+    }
