39899f896cac9d3782ae4e6b6137a4269561de04
Augusto <augustoramos067@gmail.com>
2025-04-21 22:54:57 -0300

    insert 1

---
+++ src/banco_de_dados/insert.sql
@@ -0,0 +1,5 @@
+-- Inserção de clientes
+INSERT INTO Cliente (nome, telefone, email, cpf, passaporte, tipo) VALUES
+('João Silva', '(11) 99999-9999', 'joao@email.com', '123.456.789-00', NULL, 'NACIONAL'),
+('Maria Souza', '(21) 98888-8888', 'maria@email.com', '987.654.321-00', NULL, 'NACIONAL'),
+('John Smith', '(44) 77777-7777', 'john@email.com', NULL, 'US12345678', 'ESTRANGEIRO');
